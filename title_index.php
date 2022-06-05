234ff0255f88afdf6349e6186b699bc0901d8a87
Awais Ahmad <awais_rafaqat2300@yahoo.com>
2022-06-05 13:29:17 +0500

    code updated

---
+++ title_index.php
@@ -0,0 +1,37 @@
+				<div class="row-fluid">
+
+					<div class="span12">
+
+					</div>
+
+				</div>
+				<div class="row-fluid">
+
+					<div class="span4">
+						<img class="index_logo" src="admin/images/LOGO-1.png">
+					</div>
+					<div class="span8">
+
+						<div class="title" >
+							<p class="chmsc" style="color:navy">PN SCHOOL OF MANAGEMENT</p>
+							<h3>
+
+								<p style="color:navy">PN UT SAILORS LMS</p>
+
+							</h3>
+						</div>
+
+					</div>
+				</div>
+
+				<div class="row-fluid">
+
+					<div class="span12">
+						<br>
+						<div class="motto">
+							<p>PNSM EXCELS:</p>
+							<p>Excellence, Competence and Educational</p>
+							<p>Leadership and Managment</p>
+						</div>
+					</div>
+				</div>
